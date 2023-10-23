28ee0ae94b23753f1a5128790e3c9204494dc921
Valery-Lytvyn <lytvynvalery@gmail.com>
2023-10-23 11:50:59 +0300

    Updates

---
+++ assets/FavoritesPage-148f7300.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-d2f68ca1.js";function t(){return e.jsx("div",{children:"FavoritesPage"})}export{t as default};
--- assets/FavoritesPage-6c141ab2.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-83f7d5be.js";function t(){return e.jsx("div",{children:"FavoritesPage"})}export{t as default};
